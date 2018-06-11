8a19d5e19e7bf4e0ee001ad6df5b11640576eda4
sebastien <sebastienfolliot@yahoo.fr>
2018-06-11 16:09:53 +0200

    Ajout de nouveaux services

--- app/app.php
+++ app/app.php
@@ -20,8 +20,26 @@ $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->register(new Silex\Provider\AssetServiceProvider(), array(
     'assets.version' => 'v1'
 ));
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'secured' => array(
+            'pattern' => '^/',
+            'anonymous' => true,
+            'logout' => true,
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'users' => function () use ($app) {
+                return new ClickPizza\DAO\UserDAO($app['db']);
+            },
+        ),
+    ),
+));
 
 // Enregistrer les services
 $app['dao.commodity'] = function ($app) {
     return new ClickPizza\DAO\CommodityDAO($app['db']);
 };
+$app['dao.user'] = function ($app) {
+    return new ClickPizza\DAO\UserDAO($app['db']);
+};
+
