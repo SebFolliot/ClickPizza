b15149160c7bdb80cf24316c6e33f84bdef85119
sebastien <sebastienfolliot@yahoo.fr>
2018-06-11 16:11:40 +0200

    Ajout d'une route pour le formulaire d'authentification user

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 // Page d'accueil
 $app->get('/', function () use ($app) {
     $commodities = $app['dao.commodity']->pizzaList();
@@ -15,3 +17,11 @@ $app->get('/salad', function () use ($app) {
     $commodities = $app['dao.commodity']->saladList();
     return $app['twig']->render('index.html.twig', array('commodities' => $commodities));
 })->bind('salad');
+
+// Formulaire de connexion
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('login');
