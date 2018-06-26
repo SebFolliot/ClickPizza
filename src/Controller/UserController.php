abd8adf2a1d595eb7ef8268f4c1edb99c93ac69d
sebastien <sebastienfolliot@yahoo.fr>
2018-06-26 15:48:26 +0200

    Création de la classe UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,48 @@
+<?php
+namespace ClickPizza\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use ClickPizza\Entity\User;
+use ClickPizza\Form\CreateAccountUserType;
+
+class UserController {
+    
+    /**
+     * User login controller
+     *
+     * @param Application $app Silex application
+     */
+    public function loginAction(Request $request, Application $app) {
+        return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+        'title'         => 'Page d\'authentification'
+        ));
+    }
+    
+    /**
+     * Create a user account controller
+     *
+     * @param Application $app Silex application
+     */
+    public function createAccountAction(Request $request, Application $app) {
+        $user = new User();
+        $userForm = $app['form.factory']->create(CreateAccountUserType::class, $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $salt = substr(md5(time()), 0, 23);
+            $user->setSalt($salt);
+            $simplePassword = $user->getPassword();
+            $encoder = $app['security.encoder.bcrypt'];
+            $password = $encoder->encodePassword($simplePassword, $user->getSalt());
+            $user->setPassword($password);
+            $app['dao.user']->add($user);
+            $app['session']->getFlashBag()->add('success', 'L\'utilisateur a été créé avec succès.');
+        }
+        return $app['twig']->render('user_form.html.twig', array(
+            'title' => 'Création d\'un compte',
+            'userForm' => $userForm->createView()
+        ));
+    }
+}
