1fdac051c2f46acc5bec2ba167a7b1c9038b243e
sebastien <sebastienfolliot@yahoo.fr>
2018-06-26 15:55:20 +0200

    Modification des routes suite à la création de UserController

--- app/routes.php
+++ app/routes.php
@@ -6,7 +6,7 @@ use ClickPizza\Entity\Commodity;
 use ClickPizza\Form\CreateAccountUserType;
 
 
-// Page d'accueil
+// Home page
 $app->get('/', function () use ($app) {
     $commodities = $app['dao.commodity']->commodityList('Pizza');
     return $app['twig']->render('index.html.twig', array(
@@ -39,31 +39,10 @@ $app->get('/dessert', function () use ($app) {
     ));
 })->bind('dessert');
 
-// Formulaire de connexion
-$app->get('/login', function(Request $request) use ($app) {
-    return $app['twig']->render('login.html.twig', array(
-        'error'         => $app['security.last_error']($request),
-        'last_username' => $app['session']->get('_security.last_username'),
-        'title'         => 'Page d\'authentification'
-    ));
-})->bind('login');
+// Login form
+$app->get('/login', "ClickPizza\Controller\UserController::loginAction")
+    ->bind('login');
 
-// Création d'un compte utilisateur
-$app->match('/create_account', function(Request $request) use ($app) {
-    $user = new User();
-    $userForm = $app['form.factory']->create(CreateAccountUserType::class, $user);
-    $userForm->handleRequest($request);
-    if ($userForm->isSubmitted() && $userForm->isValid()) {
-        $salt = substr(md5(time()), 0, 23);
-        $user->setSalt($salt);
-        $simplePassword = $user->getPassword();
-        $encoder = $app['security.encoder.bcrypt'];
-        $password = $encoder->encodePassword($simplePassword, $user->getSalt());
-        $user->setPassword($password);
-        $app['dao.user']->add($user);
-        $app['session']->getFlashBag()->add('success', 'L\'utilisateur a été créé avec succès.');
-     }
-    return $app['twig']->render('user_form.html.twig', array(
-        'title' => 'Création d\'un compte',
-        'userForm' => $userForm->createView()));
-})->bind('create_account');
+// Create a user account
+$app->match('/create_account', "ClickPizza\Controller\UserController::createAccountAction")
+    ->bind('create_account');
