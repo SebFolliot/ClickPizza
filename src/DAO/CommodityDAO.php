55f79c756086fa1aa2a224aac0dd0cb0f1885fe0
sebastien <sebastienfolliot@yahoo.fr>
2018-07-14 09:09:38 +0200

    Ajout de méthodes permettant le CRUD des produits

--- src/DAO/CommodityDAO.php
+++ src/DAO/CommodityDAO.php
@@ -19,7 +19,72 @@ class CommodityDAO extends DAO
             $commodities[$commodityId] = $this->buildEntityObject($row);
         }
         return $commodities;
-    } 
+    }
+    
+    /**
+     * @return \ClickPizza\Entity\Commodity or an exception if no user found
+     */
+    public function commodityId($id) {
+        $sql = "SELECT * FROM commodity WHERE com_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildEntityObject($row);
+        else
+            throw new \Exception("Aucun identifiant correspondant à ce produit" . $id);
+    }
+    
+     /**
+     * Returns a list of all commodities, sorted by role and name.
+     *
+     * @return array A list of all users.
+     */
+    public function allCommodities() {
+        $sql = "SELECT * FROM commodity ORDER BY com_type";
+        $result = $this->getDb()->fetchAll($sql);
+        // Convert query result to an array of entity objects
+        $entities = array();
+        foreach ($result as $row) {
+            $id = $row['com_id'];
+            $entities[$id] = $this->buildEntityObject($row);
+        }
+        return $entities;
+    }
+    
+    /**
+     * Add or update an commodity into the database
+     *
+     */
+    
+    public function update(Commodity $commodity) {
+        $commodityData = array(
+            'com_type' => $commodity->getType(),
+            'com_title' => $commodity->getTitle(),
+            'com_description' => $commodity->getDescription(),
+            'com_picture' => $commodity->getPicture(),
+            'com_price' => $commodity->getPrice(),
+        );
+       if($commodity->getId()) {
+            // Update if commodity already registered
+            $this->getDb()->update('commodity', $commodityData, array('com_id' => $commodity->getId()));
+        }
+        else {            
+            // Add if commodity never saved
+            $this->getDb()->insert('commodity', $commodityData);
+            // Get the id of the created commodity
+            $id = $this->getDb()->lastInsertId();
+            $commodity->setId($id);
+        }
+    
+        
+    }
+    
+    /**
+     * Delete a commodity in the database
+     */
+    public function delete($id) {
+        $this->getDb()->delete('commodity', array('com_id' => $id));
+    }
 
     
     /**
