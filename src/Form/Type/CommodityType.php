9780d3ee69d71bfdc926bffbd00cb2f71b663b41
sebastien <sebastienfolliot@yahoo.fr>
2018-07-17 19:52:45 +0200

    Ajout de contraintes au formulaire qui gère l'ajout/modif des produits

--- src/Form/Type/CommodityType.php
+++ src/Form/Type/CommodityType.php
@@ -20,17 +20,52 @@ class CommodityType extends AbstractType
             ->add('type', ChoiceType::class, array(
                 'choices' => array('Pizza' => 'Pizza', 'Salade' => 'Salade', 'Boisson' => 'Boisson', 'Dessert' => 'Dessert')))
             ->add('title', TextType::class, array(
-                'required' => true))
+                'required' => true,
+                'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                'min' => 2,
+                'max' => 60,
+                'minMessage' => 'Le nom du produit est trop court. Il faut {{ limit }} caractères minimum.',
+                'maxMessage' => 'Le nom du produit est trop long. Il faut {{ limit }} caractères maximum.'
+                ))),
+            ))
             ->add('description', TextareaType::class, array(
-                'required' => true))
+                'required' => true,
+                'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                'min' => 10,
+                'max' => 255,
+                'minMessage' => 'La description du produit est trop courte. Il faut {{ limit }} caractères minimum.',
+                'maxMessage' => 'La description du produit est trop longue. Il faut {{ limit }} caractères maximum.'
+                ))),
+            ))
             ->add('picture', FileType::class, array(
                 'required' => true,
-                'data_class' => null))
+                'data_class' => null,
+                'constraints' => array(
+                new Assert\Image(array(
+                'mimeTypes' => array(
+                    'image/jpeg', 'image/png'),
+                'mimeTypesMessage' => 'Le format du fichier image doit être au format jpeg ou au format png.'                
+                )),
+                new Assert\File(array(
+                'maxSize' => '1024k',
+                'maxSizeMessage' => 'Le fichier est trop volumineux. La taille maximale autorisée est de 1024 ko'
+                ))),
+            ))
             ->add('price', NumberType::class, array(
                 'required' => true,
                 'constraints' => array(
-                new Assert\NotBlank(),               
-                )));
+                new Assert\NotBlank(),
+                new Assert\Range(array(
+                'min' => 1,
+                'max' => 40,
+                'minMessage' => 'Le prix d\'un produit doit être au minimum de {{ limit }} €.',
+                'maxMessage' => 'Le prix d\'un produit doit être au maximum de {{ limit }} €.'
+                ))),
+                ));
     }
     
 
