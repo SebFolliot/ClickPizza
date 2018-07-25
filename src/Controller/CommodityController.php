6784008b5855945c71d6370d38bbad742d3f8a1b
sebastien <sebastienfolliot@yahoo.fr>
2018-07-25 18:12:21 +0200

    Mise à jour du controller pour l'entité des produits

---
+++ src/Controller/CommodityController.php
@@ -0,0 +1,69 @@
+<?php
+namespace ClickPizza\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use ClickPizza\Entity\Commodity;
+use ClickPizza\Form\Type\CommodityType;
+
+class CommodityController 
+{
+    
+    /**
+     * Add a product controller
+     *
+     * @param Application $app Silex application
+     */
+    public function addCommodityAction (Request $request, Application $app) {
+        $commodity = new Commodity();
+        $commodityForm = $app['form.factory']->create(CommodityType::class, $commodity);
+        $commodityForm->handleRequest($request);
+        if ($commodityForm->isSubmitted() && $commodityForm->isValid()) {
+            $directory = __DIR__.'/../web/images/upload';
+            $file = $commodityForm['picture']->getData();
+            $file->move($directory, $file->getClientOriginalName());
+            $commodity->setPicture($file->getClientOriginalName());
+            $app['dao.commodity']->update($commodity);
+            $app['session']->getFlashBag()->add('success', 'Le produit a été mis à jour sur la carte.');
+        }
+        return $app['twig']->render('commodity_form.html.twig', array(
+        'title' => 'Ajout d\'un produit',
+        'commodityForm' => $commodityForm->createView()));
+    }
+    
+    /**
+     * Update a product controller
+     *
+     * @param integer $id User id
+     * @param Application $app Silex application
+     */    
+    public function editCommodityAction ($id, Request $request, Application $app) {
+        $commodity = $app['dao.commodity']->commodityId($id);
+        $commodityForm = $app['form.factory']->create(CommodityType::class, $commodity);
+        $commodityForm->handleRequest($request);
+        if ($commodityForm->isSubmitted() && $commodityForm->isValid()) {
+            $directory = __DIR__.'/../web/images/upload';
+            $file = $commodityForm['picture']->getData();
+            $file->move($directory, $file->getClientOriginalName());
+            $commodity->setPicture($file->getClientOriginalName());
+            $app['dao.commodity']->update($commodity);
+            $app['session']->getFlashBag()->add('success', 'Le produit a été mis à jour avec succès.');
+        }
+        return $app['twig']->render('commodity_form.html.twig', array(
+            'title' => 'Modifier le produit',
+            'commodityForm' => $commodityForm->createView()));
+    }
+    
+    /**
+     * Delete a product controller
+     *
+     * @param integer $id User id
+     * @param Application $app Silex application
+     */
+    public function deleteCommodityAction ($id, Request $request, Application $app) {
+        $app['dao.commodity']->delete($id);
+        $app['session']->getFlashBag()->add('success', 'Le produit a été supprimé de la base de données.');
+        // Redirect to admin home page
+        return $app->redirect($app['url_generator']->generate('admin'));
+    }
+}
