1eaac58fe7088deefe064c2db999b111ace393d8
sebastien <sebastienfolliot@yahoo.fr>
2018-09-11 09:50:41 +0200

    Modification de la méthode addOrderAction avec la prise en compte d'OrderCommodity

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace ClickPizza\Controller;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use ClickPizza\Entity\Order;
+use ClickPizza\Entity\OrderCommodity;
     
 class OrderController
 {
@@ -14,52 +15,70 @@ class OrderController
      */
     public function addOrderAction (Application $app) {
         $order = new Order();
-        if (isset($_COOKIE['caddyProducts']) && isset($_COOKIE['caddyUser']) && isset($_COOKIE['caddyPrice']) && $_COOKIE['caddyProductsNumber']) {
+
+        if (isset($_COOKIE['caddyProducts']) && isset($_COOKIE['caddyUser']) && isset($_COOKIE['caddyPrice']) && isset($_COOKIE['caddyComId'])) {
+            
             $cookie_userId = $_COOKIE['caddyUser'];
             $removebase64UserId = base64_decode($cookie_userId);
             $userId_record = json_decode($removebase64UserId, true);
+    
             $user = $app['dao.user']->userList($userId_record);
             
-            $cookie_products = $_COOKIE['caddyProducts'];
-            $products_record = utf8_encode(base64_decode($cookie_products)); 
-            
-
-            $test = json_decode($products_record, true);
-                    
             $cookie_price = $_COOKIE['caddyPrice'];
             $removebase64Price = base64_decode($cookie_price);
             $price_record = json_decode($removebase64Price, true);
-    
+            
             $status = 'En cours';
+        
+            $cookie_products = $_COOKIE['caddyProducts'];
+            $products = utf8_encode(base64_decode($cookie_products));
             
+            $products_record = json_decode($products, true);
+                     
             $order->setUser($user);
-
-            if (isset($test)) {
-                foreach ($test as $i => $v) {
-                    $products[$i] = $v['qt'] . ' ' . $v['name'] . '<br />';
-                    $order->setContent($products[$i]);        
-                }
-            }  
-            
             $order->setStatus($status);
             $order->setPrice($price_record);
+                      
+            $app['dao.order']->add($order);
             
-            
+            $cookie_comId = $_COOKIE['caddyComId'];
+            $removebase64ComId = base64_decode($cookie_comId);
+            $comId_record = json_decode($removebase64ComId, true);
+                
+            // instantiation of an OrderCommodity object
+            $orderCommodity = new OrderCommodity();
+             
+            foreach ($products_record as $v) {
+                    
+                // We recover the products
+                $commodity = $app['dao.commodity']->commodityId($v['id']);  
+                            
+                // We link it to the order
+                $orderCommodity->setOrder($order);
+                              
+                // We link it to the commodity
+                $orderCommodity->setCommodity($commodity);
+                    
+                $quantity = $v['qt'];
+                
+                // each commodity is with so much amount
+                $orderCommodity->setQuantity($quantity);
+
+                $app['dao.orderCommodity']->add($orderCommodity);
+            } 
+                              
             $number_order = $user->getOrderNumber();
             $number_order++;
             $user->setOrderNumber($number_order); 
-            
-            $app['dao.order']->add($order);
-            
+                   
             $app['dao.user']->updateOrderNumber($user);
             
             setcookie('caddyProductsNumber', 0, 1 * 24 * 60 * 60 * 1000);
             setcookie('caddyProducts', '', 1 * 24 * 60 * 60 * 1000);
-            
-        }
+       }
         return $app['twig']->render('order.html.twig', array(
-        'title' => 'Confirmation de commande'            
-        ));
+        'title' => 'Confirmation de commande'
+            ));        
     }
     
     /**
