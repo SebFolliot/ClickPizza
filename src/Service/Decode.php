479f5bd02258437239f456497998623cc3576f0b
sebastien <sebastienfolliot@yahoo.fr>
2018-11-05 10:35:19 +0100

    Suppression d'un  user inutile

--- src/Service/Decode.php
+++ src/Service/Decode.php
@@ -4,11 +4,21 @@ namespace ClickPizza\Service;
 
 class Decode 
 {
+     /**
+     *
+     * @param integer or string $cookie
+     * @return a decoded string in base64 MIME
+     */
     public function base64Decode($cookie) {
         $cookieBase64Decode = utf8_encode(base64_decode($cookie));
         return $cookieBase64Decode;
     }
     
+     /**
+     *
+     * @param a decoded string in base64 MIME
+     * @return a decoded string JSON
+     */
     public function jsonDecode($cookie) {
         $cookieBase64Decode = $this->base64Decode($cookie);
         $cookieJsonDecode = json_decode($cookieBase64Decode, true);
