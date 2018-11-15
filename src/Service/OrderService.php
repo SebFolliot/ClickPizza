edea6349ea57de03e9751d9271cd1b4019f1ee40
sebastien <sebastienfolliot@yahoo.fr>
2018-11-15 15:02:25 +0100

    Ajout de la méthode orderListView à la classe OrderService pour la gestion de la vue des commandes en fonction du moteur de recherche

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -68,4 +68,77 @@ class OrderService
         // Sending the order by mail
         $app['service.email']->emailAddOrder($order, $user, $message, $price_record);
     }
+    
+    /**
+     * @returns the view to display after searching for commands by filter 
+     *
+     * @param $users
+     * @param $commodities
+     * @param $ordersCommodities
+     * @param $formSearchOrder
+     * @param Application $app Silex application
+     */
+    public function orderListView($users, $commodities, $ordersCommodities, $formSearchOrder, Application $app) {
+        
+        $data = $formSearchOrder->getData();
+        $numberOfPages = $app['dao.order']->numberOfPagesForStatusOrders($data['status'], $app);
+        $currentPage = 1;
+        $orders = $app['dao.order']->searchListOrders($data['status'], $currentPage, $app);
+                     
+        if($data['status'] || (($app['dao.order']->orderList($data['id']) !== null) && $data['status'])) {
+           if (($app['dao.order']->orderList($data['id']) === null) && isset($data['id'])) {
+               if($data['status'] === 'Toutes') {
+                   $numberOfPages = $app['dao.order']->numberOfPagesForOrders($app);
+               }
+               $app['session']->getFlashBag()->add('warning', 'La commande n° '.$data['id'].' n\'existe pas');
+       
+               $view = $app['twig']->render('list_order.html.twig', array(
+                'users' => $users,
+                'status' => $data['status'],
+                'formSearchOrder' => $formSearchOrder->CreateView(),
+                'commodities' => $commodities,
+                'orders' => $orders,
+                'numberOfPages' => $numberOfPages,
+                'currentPage' => $currentPage,
+                'ordersCommodities' => $ordersCommodities,
+                'title' => 'Les commandes'));
+               return $view; 
+           } 
+                
+           if(isset($data['id'])) {     
+                $thisStatus = $app['dao.order']->orderList($data['id'])->getStatus();
+                if (($thisStatus !== $data['status']) && ($data['status'] !== 'Toutes')) {
+                    $app['session']->getFlashBag()->add('warning', 'La commande n° '.$data['id']. ' n\'est pas une commande ' .mb_strtolower($data['status']).' mais une commande '.mb_strtolower($thisStatus));
+                        
+                    $view = $app['twig']->render('list_order.html.twig', array(
+                        'users' => $users,
+                        'status' => $data['status'],
+                        'formSearchOrder' => $formSearchOrder->CreateView(),
+                        'commodities' => $commodities,
+                        'orders' => $orders,
+                        'numberOfPages' => $numberOfPages,
+                        'currentPage' => $currentPage,
+                        'ordersCommodities' => $ordersCommodities,
+                        'title' => 'Les commandes'));
+                    return $view;
+                }
+           }
+                
+           $numberOfPages = $app['dao.order']->numberOfPagesWithSearchOrders($data, $app);
+           $currentPage = 1;
+           $orders = $app['dao.order']->searchListOrders($data, $currentPage, $app);
+           $view = $app['twig']->render('list_order.html.twig', array(
+               'users' => $users,
+               'status' => $data['status'],
+               'id' => $data['id'],
+               'formSearchOrder' => $formSearchOrder->CreateView(),
+               'commodities' => $commodities,
+               'orders' => $orders,
+               'numberOfPages' => $numberOfPages,
+               'currentPage' => $currentPage,
+               'ordersCommodities' => $ordersCommodities,
+               'title' => 'Les commandes'));
+           return $view;
+        }  
+    }
 } 
